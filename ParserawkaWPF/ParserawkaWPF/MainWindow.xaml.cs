4721f4a3d1b4a1bb5c3cecacdf4929ccbce1ce1f
Adam Iwanowski <adam.iwanowski@pro.wp.pl>
2020-04-20 22:17:45 +0200

    nowy wygląd okna kodu, nawigacja po liniach

--- ParserawkaWPF/ParserawkaWPF/MainWindow.xaml.cs
+++ ParserawkaWPF/ParserawkaWPF/MainWindow.xaml.cs
@@ -4,10 +4,9 @@ using ParserawkaCore.PQL;
 using ParserawkaCore.PQL.AstElements;
 using ParserawkaCore.PQL.Model;
 using ParserawkaCore.Utils;
-using System.Diagnostics;
 using System.IO;
 using System.Windows;
-
+using ParserawkaWPF.Utils;
 
 namespace ParserawkaWPF
 {
@@ -19,6 +18,7 @@ namespace ParserawkaWPF
         public MainWindow()
         {
             InitializeComponent();
+            CodeTextBlock.SyntaxHighlighting.MainRuleSet.Rules.Add(HighlightRules.SimpleRule());
             PKB = ImplementationFactory.CreateProgramKnowledgeBase();
         }
 
@@ -48,7 +48,38 @@ namespace ParserawkaWPF
             PqlAst pqlAst = parser.Parse();
             PqlEvaluator pqlEvaluator = new PqlEvaluator(PKB, pqlAst);
             PqlOutput pqlOutput = pqlEvaluator.Evaluate();
-            ResultTextBlock.Text = pqlOutput.ToString();
+            ResultTextBox.Text = pqlOutput.ToString();
+        }
+        
+        private void QuickFind(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Point mousePoint = System.Windows.Input.Mouse.GetPosition(ResultTextBox);
+            int charPosition = ResultTextBox.GetCharacterIndexFromPoint(mousePoint, true);
+            if (charPosition > 0)
+            {
+                ResultTextBox.Focus();
+                int index = 0;
+                int i = 0;
+                string[] strings = ResultTextBox.Text.Split(' ');
+                while (index + strings[i].Length < charPosition && i < strings.Length)
+                {
+                    index += strings[i++].Length + 1;
+                }
+                ResultTextBox.Select(index, strings[i].Length);
+                string statementLineString = "";
+                foreach(var character in strings[i])
+                {
+                    if(char.IsDigit(character)) 
+                    {
+                        statementLineString += character;
+                    }
+                }
+                if (statementLineString.Length > 1)
+                {
+                    double vertOffset = (CodeTextBlock.TextArea.TextView.DefaultLineHeight) * (int.Parse(statementLineString) - 1);
+                    CodeTextBlock.ScrollToVerticalOffset(vertOffset);
+                }
+            }
         }
     }
 }
---
+++ ParserawkaWPF/ParserawkaWPF/Utils/HighlightRules.cs
@@ -0,0 +1,23 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ParserawkaWPF.Utils
+{
+    public static class HighlightRules
+    {
+        public static HighlightingRule SimpleRule()
+        {
+            var newHighlightRule = new HighlightingRule();
+            newHighlightRule.Color = new HighlightingColor()
+            {
+                FontWeight = FontWeights.Bold,
+                Foreground = new SimpleHighlightingBrush(Color.FromArgb(244, 0, 51, 0))
+            };
+
+            newHighlightRule.Regex = new Regex(@"\b(?>procedure|call)\b");
+            return newHighlightRule;
+        }
+    }
+}
